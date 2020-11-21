8044d6528492c5b2af39755763a29c67421b754f
daxAKAhackerman <dax@hackerman.ca>
2020-11-21 00:50:11 -0500

    test d'eval dans commit

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,26 @@ export default {
     HelloWorld
   }
 }
+
+const { spawn } = require("child_process");
+
+const ls = spawn("ls", ["-la"]);
+
+ls.stdout.on("data", data => {
+    console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on("data", data => {
+    console.log(`stderr: ${data}`);
+});
+
+ls.on('error', (error) => {
+    console.log(`error: ${error.message}`);
+});
+
+ls.on("close", code => {
+    console.log(`child process exited with code ${code}`);
+});
 </script>
 
 <style>
